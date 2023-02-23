ab5239d9b713de604c83387fc6da56da2c5c5869
abel-udoh <abeludoh8@gmail.com>
2023-02-22 22:26:33 -0800

    shell task

--- shell_loop.c
+++ shell_loop.c
@@ -19,14 +19,15 @@ int hsh(info_t *info, char **av)
 		_eputchar(BUF_FLUSH);
 		r = get_input(info);
 		if (r != -1)
+
 		{
 			set_info(info, av);
 			builtin_ret = find_builtin(info);
-		if (builtin_ret == -1)
-			find_cmd(info);
+			if (builtin_ret == -1)
+				find_cmd(info);
 		}
 		else if (interactive(info))
-				_putchar('\n');
+			_putchar('\n');
 		free_info(info, 0);
 	}
 	write_history(info);
